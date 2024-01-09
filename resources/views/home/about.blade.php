e270dd9e4ed8bdd678579512d5f4dced0e3e2034
unknown <tasrifaadman@gmail.com>
2024-01-09 21:26:02 +0000

    kerjakan_about

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -1,1 +1,45 @@
-about.blade.php
+<!-- @extends('master_inner')
+
+@section('content')
+	<section class="inner_page ">
+		<div class="container">
+			<p>
+			    Example inner page template
+			</p>
+		</div>
+	</section>
+@endsection -->
+
+@extends('master.master_inner')
+@section('content')
+    <section id="about" class="about">
+
+      <div class="container" data-aos="fade-up">
+        <div class="row gx-0" style="margin-top: 50px;">
+
+          <div class="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200" >
+            <div class="content">
+              <h3>Who We Are</h3>
+              <h2>Expedita voluptas omnis cupiditate totam eveniet nobis sint iste. Dolores est repellat corrupti reprehenderit.</h2>
+              <p>
+                Quisquam vel ut sint cum eos hic dolores aperiam. Sed deserunt et. Inventore et et dolor consequatur itaque ut voluptate sed et. Magnam nam ipsum tenetur suscipit voluptatum nam et est corrupti.
+              </p>
+              <div class="text-center text-lg-start">
+                <a href="#" class="btn-read-more d-inline-flex align-items-center justify-content-center align-self-center">
+                  <span>Read More</span>
+                  <i class="bi bi-arrow-right"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
+            <img src="theme/home/assets/img/about.jpg" class="img-fluid" alt="">
+          </div>
+
+        </div>
+      </div>
+
+    </section><!-- End About Section -->
+
+@endsection
