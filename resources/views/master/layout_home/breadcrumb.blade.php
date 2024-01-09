0ce719b87b7753dee09d9c8216288dc408d3fa7d
AmyQadri <qodristr@gmail.com>
2024-01-09 20:59:41 +0800

    kerjakan master

---
+++ resources/views/master/layout_home/breadcrumb.blade.php
@@ -0,0 +1,9 @@
+    <section class="breadcrumbs">
+      <div class="container">
+        <ol>
+          <li><a href="index.html">Home</a></li>
+          <li>Inner Page</li>
+        </ol>
+        <h2>Inner Page</h2>
+      </div>
+    </section>
