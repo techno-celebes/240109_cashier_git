0b3cf8e52ef17355405e24c9ac2596b6556a0a80
Techno Celebes <official@techno-celebes.com>
2024-01-11 16:58:30 +0800

    terbaru

---
+++ app/Http/Controllers/PanelController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PanelController extends Controller
+{
+    public function dashboard(){
+        return view("panel.dashboard");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -19,15 +19,20 @@ Route::get('/', "BerandaController@home")->name("home");
 Route::get('/about', "BerandaController@about")->name("about");
 Route::get('/pricing', "BerandaController@pricing")->name("pricing");
 Route::get('/contact', "BerandaController@contact")->name("contact");
-Route::get('/customer', 'CustomerController@customer')->name("create-customer-get");
-Route::get('/stock', 'StockController@stock')->name("create-stock-get");
-Route::get('/transaction', 'TransactionController@transaction')->name("transaction");
 
-Route::post('create-customer','CustomerController@create_customer')->name('create-customer');
-Route::post('create-stock','StockController@create_stock')->name('create-stock');
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::prefix('panel')->group(function(){
+    Route::get('/', 'PanelController@dashboard')->name("dashboard");
+    Route::get('/customer', 'CustomerController@customer')->name("create-customer-get");
+    Route::get('/stock', 'StockController@stock')->name("create-stock-get");
+    Route::get('/transaction', 'TransactionController@transaction')->name("transaction");
 
-Route::prefix('/customers/stock')->group(function(){
-    Route::get('/view{id}', 'StockController@view_single');
+    Route::post('create-customer','CustomerController@create_customer')->name('create-customer');
+    Route::post('create-stock','StockController@create_stock')->name('create-stock');
+
+    Route::get('/home', 'HomeController@index')->name('home');
+
+    Route::prefix('/customers/stock')->group(function(){
+        Route::get('/view{id}', 'StockController@view_single');
+    });
 });
