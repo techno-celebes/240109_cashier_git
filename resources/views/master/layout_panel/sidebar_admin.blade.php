157926c40d0ae8f023e3ee4972a898f380103109
Ryu9145 <mohgiansaputra@gmail.com>
2024-01-11 20:13:21 +0700

    remake admin

--- resources/views/master/layout_panel/sidebar_admin.blade.php
+++ resources/views/master/layout_panel/sidebar_admin.blade.php
@@ -3,7 +3,7 @@
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="{{ asset('../../theme/home/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Halaman admin</span>
+      <span class="brand-text font-weight-light">Halaman Admin</span>
     </a>
 
     <!-- Sidebar -->
