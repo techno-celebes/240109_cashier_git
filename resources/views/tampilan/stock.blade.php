0b1c44940ef4b7a895a8254edbe2cd7198969059
Techno Celebes <official@techno-celebes.com>
2024-01-12 20:36:08 +0800

    mantap jiwa

--- resources/views/tampilan/stock.blade.php
+++ resources/views/tampilan/stock.blade.php
@@ -6,25 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-<form method="post" action = "{{route('create-stock')}}">
-    {{ @csrf_field()}}
-    <label>nama barang</label>
-    <input type="text" name ="namabarang">
 
-    <label>harga jual</label>
-    <input type="text" name ="hargajual">
-    
-    <label>harga beli</label>
-    <input type="text" name ="hargabeli">
-    <button type="submit">tambah</button>
-</form>
-
-@foreach($stock as $s)
- <h1>nama barang : {{ $s->nama_barang }}</h1>
- <h1>harga beli: {{ $s->harga_beli}}</h1>
- <h1>harga jual: {{ $s->harga_jual}}</h1>
-@endforeach
-{{ $stock->links() }}
     
 </body>
 </html>
